f3787430d6f8ec27b82e5748dfc4e1ac338327c9
Jérémy Lal <kapouer@melix.org>
2022-11-06 18:07:50 +0100

    Upgrade compare.js

--- test/compare.js
+++ test/compare.js
@@ -1,4 +1,4 @@
-import {promises as fs} from 'fs';
+import { promises as fs } from 'fs';
 import * as Path from 'path';
 import { promisify } from 'util';
 import { exec } from 'child_process';
@@ -26,10 +26,8 @@ async function convert(name, str) {
 		date: '1979-01-01',
 		breaks: true
 	});
-	let manPath = Path.join(manDir, name);
-	const status = await writeOrCompare(roff, manPath);
-	if (status < 0) manPath += '.err';
-	const { stdout, stderr } = await execa(`man --warnings --encoding=UTF-8 ${manPath}.1`, {
+	const [status, manPath] = await writeOrCompare(roff, manDir, name + ".1");
+	const { stdout, stderr } = await execa(`man --warnings --encoding=UTF-8 ${manPath}`, {
 		env: {
 			LC_ALL: "C.UTF-8",
 			MAN_KEEP_FORMATTING: '1',
@@ -37,7 +35,7 @@ async function convert(name, str) {
 		}
 	});
 	if (stderr) console.error(manPath, '\n', stderr);
-	return stdout;
+	return [status, stdout];
 }
 
 async function main() {
@@ -45,6 +43,14 @@ async function main() {
 	let fails = 0,
 		works = 0,
 		news = 0;
+	try {
+		await fs.rm(manDir + "-err", { recursive: true });
+		await fs.rm(outDir + "-err", { recursive: true });
+	} catch (err) {
+		// pass
+	}
+	await fs.mkdir(manDir + "-err", { recursive: true });
+	await fs.mkdir(outDir + "-err", { recursive: true });
 	await Promise.all(files.map(async file => {
 		const status = await check(file);
 		switch (status) {
@@ -62,33 +68,39 @@ async function main() {
 	if (fails > 0) console.error("Failed tests: ", fails);
 	if (works > 0) console.info("Succeeded tests: ", works);
 	if (news > 0) console.info("New tests: ", news);
-	if (fails == 0) console.info("All tests passed");
-	else process.exit(1);
+	if (fails == 0) {
+		console.info("All tests passed");
+		await fs.rm(manDir + "-err", { recursive: true });
+		await fs.rm(outDir + "-err", { recursive: true });
+	} else {
+		process.exit(1);
+	}
 }
 
-async function writeOrCompare(str, path) {
+async function writeOrCompare(str, root, name) {
+	const okpath = Path.join(root, name);
+	const errpath = Path.join(root + "-err", name);
 	let status = 0;
-	const okpath = path + ".1";
 	try {
 		const expect = (await fs.readFile(okpath)).toString();
-		const errpath = path + '.err.1';
 		if (expect != str) {
 			console.error("Test failure, result written in", errpath);
+			await fs.writeFile(errpath, str);
 			status = -1;
 		}
-		await fs.writeFile(errpath, str);
-	} catch(e) {
+	} catch (e) {
+		console.info("Missing test, result written in", okpath);
 		await fs.writeFile(okpath, str);
 		status = 1;
 	}
-	return status;
+	return [status, status >= 0 ? okpath : errpath];
 }
 
 async function check(filename) {
 	const ronnBuf = await fs.readFile(Path.join(ronnDir, filename));
 	const name = Path.basename(filename, Path.extname(filename));
-	const destPath = Path.join(outDir, name);
-	const output = await convert(name, ronnBuf.toString());
-	const status = await writeOrCompare(output, destPath);
-	return status;
+	const [statusMan, output] = await convert(name, ronnBuf.toString());
+	if (statusMan != 0) return statusMan;
+	const [statusOut] = await writeOrCompare(output, outDir, name);
+	return statusOut;
 }
